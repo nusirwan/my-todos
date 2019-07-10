a710880defa7de8da4c27f2b4eaa2238d0da7976
nusirwan <inbox@iwan.im>
2019-07-10 22:15:22 +0700

    Set init todos to State & passing to item list

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -2,12 +2,39 @@ import React, { Component } from 'react';
 
 import { Todos, Wraper } from './styles';
 
+const initTodos = [
+	{
+		id: 0,
+		task: 'Buy running shoes',
+		completed: false,
+	},
+	{
+		id: 1,
+		task: 'Run two miles with Lisa',
+		completed: true,
+	},
+];
+
 class TodoList extends Component {
+	state = {
+		todos: initTodos,
+	};
+
 	render() {
+		const { todos } = this.state;
+
 		return (
 			<Wraper>
 				<Todos>
-					Todo List
+					{ todos.map( todo => (
+						<li
+							key={ todo.id }
+							id={ todo.id }
+							task={ todo.task }
+						>
+							{ todo.task }
+						</li>
+					) ) }
 				</Todos>
 			</Wraper>
 		);
