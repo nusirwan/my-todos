4acd682a3b508ded484b1ee612748f71207ac71c
nusirwan <inbox@iwan.im>
2019-07-11 03:49:15 +0700

    Todo: set content width & import breakpoints from base-styles

--- src/components/TodoList/styles.js
+++ src/components/TodoList/styles.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 
+import theme from '../../base-styles/theme';
+
+const { breakpoints } = theme;
+
 export const Wraper = styled.div`
+	width: 17.5rem;
 	margin-top: 10%;
 	margin-left: auto;
 	margin-right: auto;
+
+	@media ( ${ breakpoints.mobile } ) {
+		width: 24rem;
+	}
 `;
 
 export const Todos = styled.ul`
-	display: block;
+	padding-left: 0;
 `;
