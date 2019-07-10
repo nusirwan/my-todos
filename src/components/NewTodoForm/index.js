0cd3fab3aadd1df15d34c0e8e3e6093d82b33263
nusirwan <inbox@iwan.im>
2019-07-10 22:42:40 +0700

    Add component newTodoForm

---
+++ src/components/NewTodoForm/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { FiCornerRightDown } from 'react-icons/fi';
+
+import { Form, Input, Submit } from './styles';
+
+const NewTodoForm = props => {
+	const [ task, setTask ] = useState( '' );
+	const { addTodo, isVisible } = props;
+
+	const handleChange = event => {
+		setTask( event.target.value )
+	}
+
+	const handleSubmit = event => {
+		event.preventDefault();
+		addTodo( task );
+		setTask( '' );
+	}
+
+	return (
+		<Form onSubmit={ handleSubmit } isVisible={ isVisible }>
+			<Input
+				autoComplete="off"
+				autoFocus={ true }
+				id='task'
+				name='task'
+				onChange={ handleChange }
+				placeholder='New Todo'
+				type="text"
+				value={ task }
+			/>
+			<Submit disabled={ ! task }><FiCornerRightDown /></Submit>
+		</Form>
+	)
+}
+
+export default NewTodoForm;
