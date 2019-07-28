e8cd98fe4a07e536bff064e777e996f2c6c49934
nusirwan <inbox@iwan.im>
2019-07-29 06:34:20 +0700

    Import & Use Provider & createStore

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, withRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
 import * as serviceWorker from './serviceWorker';
+import reducer from './store/reducers/todos';
 import App from './App';
 
+const store = createStore( reducer );
 const Main = withRouter( props => <App { ...props } /> );
+const root = document.getElementById( 'root' );
 
 ReactDOM.render(
-	<BrowserRouter>
-		<Main />
-	</BrowserRouter>,
-
-	document.getElementById( 'root' ),
+	<Provider store={ store }>
+		<BrowserRouter>
+			<Main />
+		</BrowserRouter>
+	</Provider>,
+	root,
 );
 
 // If you want your app to work offline and load faster, you can change
