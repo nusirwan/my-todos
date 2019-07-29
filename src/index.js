7798c978beb8858c6086fe3d749dc8feb24d01da
nusirwan <inbox@iwan.im>
2019-07-29 09:24:51 +0700

    Import & Use applyMiddleware & thunk

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, withRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { BrowserRouter, withRouter } from 'react-router-dom';
 
 import * as serviceWorker from './serviceWorker';
 import reducer from './store/reducers/todos';
 import App from './App';
 
-const store = createStore( reducer );
+const store = createStore( reducer, applyMiddleware( thunk ) );
 const Main = withRouter( props => <App { ...props } /> );
 const root = document.getElementById( 'root' );
 
