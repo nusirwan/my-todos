89ac695870e24058d7df8e96577a891e84f407ce
nusirwan <inbox@iwan.im>
2019-08-02 17:23:41 +0700

    TodoList: set mapDispatchToProps

--- src/components/TodoList/index.js
+++ src/components/TodoList/index.js
@@ -13,25 +13,19 @@ import { ErrorText, Todos } from './styles';
 
 class TodoList extends Component {
 	componentDidMount() {
-		this.props.dispatch( fetchTodos() );
+		this.props.fetchTodos();
 	}
 
 	update = ( id, updateTask ) => {
-		this.props.dispatch(
-			handleEditTaskTodo( id, updateTask )
-		);
+		this.props.handleEditTaskTodo( id, updateTask );
 	}
 
 	remove = id => {
-		this.props.dispatch(
-			handleRemoveTodo( id )
-		);
+		this.props.handleRemoveTodo( id );
 	}
 
 	toggleCompletion = ( id, completed ) => {
-		this.props.dispatch(
-			handleEditCompletionTodo( id, completed )
-		);
+		this.props.handleEditCompletionTodo( id, completed )
 	}
 
 	render() {
@@ -76,9 +70,23 @@ const getTodos = ( todos, filter ) => {
 	}
 }
 
+const mapDispatchToProps = dispatch => ( {
+	fetchTodos: () => dispatch( fetchTodos() ),
+	handleEditCompletionTodo: ( id, completed ) => dispatch(
+		handleEditCompletionTodo( id, completed )
+	),
+	handleRemoveTodo: id => dispatch( handleRemoveTodo( id ) ),
+	handleEditTaskTodo: ( id, updateTask ) => dispatch(
+		handleEditTaskTodo( id, updateTask )
+	),
+} );
+
 const mapStateToProps = state => ( {
 	error: state.error,
 	todos: getTodos( state.todos, state.filterTodos ),
 } );
 
-export default connect( mapStateToProps )( TodoList );
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)( TodoList );
